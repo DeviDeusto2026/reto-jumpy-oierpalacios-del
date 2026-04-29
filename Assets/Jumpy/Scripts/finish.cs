1b56d2552ebd6ae5361f1a395394c16fce11c401
oierpalacios-del <oier.palacios@opendeusto.es>
2026-04-29 08:54:01 +0200

    Arreglado bug de finish

--- Assets/Jumpy/Scripts/finish.cs
+++ Assets/Jumpy/Scripts/finish.cs
@@ -1,20 +1,15 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class finish : MonoBehaviour
 {
-    [SerializeField] private GameObject boton;
+    [SerializeField] private GameObject texto;
     private void Start()
     {
-        Time.timeScale = 1;
-        boton.SetActive(false);
-        boton.GetComponent<Button>().onClick.AddListener(()  => Application.Quit());
+        texto.SetActive(false);
     }
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        Time.timeScale = 0;
-        boton.SetActive(true);
-        Debug.Log("Finished");
+        texto.SetActive(true);
     }
 }
